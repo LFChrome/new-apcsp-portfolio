cfc774e31f90cf206178423662a3bcd7aa34be00
unknown <wuwj2001@gmail.com>
2019-01-26 08:57:16 -0800

    hello world

---
+++ src/js/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Unit } from './Unit';
+
+export class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Unit/>
+      </div>
+    );
+  }
+}
---
+++ src/js/Unit.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import { Title } from './Title';
+
+export class Unit extends Component {
+  render() {
+    return (
+      <div className="row">
+        <Title text={"Unit 5 - Algorithms & Procedural Abstraction"}/>
+        <hr></hr>
+        <div className="row">
+          <div className="col-md-3">
+
+          </div>
+          <div className="col-md-9">
+
+          </div>  
+        </div>
+      </div>
+    );
+  }
+}
